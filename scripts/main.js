d3c94c027a67ab394d9a75b5d66683c10baa3a5b
roll <roll@respect31.com>
2015-03-03 14:09:59 +0300

    implemented interactions on select draft

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
     
   var hash = $(location).attr('hash')
   if (hash) {
     scrollToElement(hash);
   }
  
-  $('#page-menu a').click(function() {
+  $('#page-menu a').click(function () {
     var hash = $(this).attr('href').replace('/', '')
     scrollToElement(hash);
     return false;
@@ -19,30 +19,44 @@ $(document).ready(function() {
     }, duration);
   }
   
-  var pageMenu = $("#page-menu");
-  var menuItems = pageMenu.find("a");
-  var scrollItems = menuItems.map(function() {
-    var item = $($(this).attr("href").replace('/', ''));
-    if (item.length) { return item; }
-  });
-    
-  $(window).scroll(function() {
-    // Get container scroll position
-    var offset = $('main').offset().top + 100
-    var fromTop = $(this).scrollTop()+offset;
-  
-    // Get id of current scroll item
-    var cur = scrollItems.map(function(){
-      if ($(this).offset().top < fromTop) {
-        return this;
-      }
+  if ($(location).attr('pathname') == '/') {
+    var pageMenu = $("#page-menu");
+    var menuItems = pageMenu.find("a");
+    var scrollItems = menuItems.map(function () {
+      var item = $($(this).attr("href").replace('/', ''));
+      if (item.length) { return item; }
     });
-    // Get the id of the current element
-    cur = cur[cur.length-1];
-    var id = cur && cur.length ? cur[0].id : "";
-    // Set/remove active class
-    menuItems.parent().removeClass("active");
-    menuItems.filter("[href='/#"+id+"']").parent().addClass("active");
-  });
+      
+    $(window).scroll(function () {
+      // Get container scroll position
+      var offset = $('main').offset().top + 100
+      var fromTop = $(this).scrollTop()+offset;
+    
+      // Get id of current scroll item
+      var cur = scrollItems.map(function (){
+        if ($(this).offset().top < fromTop) {
+          return this;
+        }
+      });
+      // Get the id of the current element
+      cur = cur[cur.length-1];
+      var id = cur && cur.length ? cur[0].id : "";
+      // Set/remove active class
+      menuItems.parent().removeClass("active");
+      menuItems.filter("[href='/#"+id+"']").parent().addClass("active");
+    });
+  }
+  
+  $('main .solution .options li').click(function () {
+    var self = $(this);
+    var target = self.parentsUntil('.solution').parent();
+    target.find('h3 .name').html(self.data('name'));
+    target.find('img').attr('src', self.data('image'));
+    target.find('h3 a').attr('href', self.data('href'));
+    target.find('.price .value').html(self.data('price'));
+    self.siblings().removeClass("active");
+    self.addClass("active");
+    return false;
+  })
   
 });
